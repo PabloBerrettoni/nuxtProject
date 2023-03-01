eb78a8cedd1a02c35c53f1c3835b29be31190276
PabloBerrettoni <pablo_berrettoni@hotmail.com>
2023-03-01 01:31:29 -0300

    pokemon stats represented in a bar chart way, and started work in the detail view

--- components/PokemonDetail.vue
+++ components/PokemonDetail.vue
@@ -1,14 +1,53 @@
 <template>
     <div>
-        <div v-if="succeded[0] == 200">
-            <img :src=" pokemons[0].pkmSprite "/>
-            <p> Name: {{ pokemons[0].pkmName  }} </p>
-            <p> Type1: {{ pokemons[0].pkmType1  }} </p>
-            <p> Type2: {{ pokemons[0].pkmType2 }} </p>
-            <p> Health: {{ pokemons[0].pkmHealth  }} </p>
-            <p> Attack: {{ pokemons[0].pkmAttack  }} </p>
-            <p> Defense: {{ pokemons[0].pkmDefense  }} </p>
-            <p> Speed: {{ pokemons[0].pkmSpeed  }} </p>
+        <div v-if="succeded[0] == 200" class="container-pokemon-detail">
+            <div class="left-sprite">
+                <img :src=" pokemons[0].pkmSprite " id="sprite-contained" />
+            </div>
+            <div class="right-info">
+                <div class="name-class">
+                    <div class="name-class-inner">
+                        <h3>Name</h3>
+                        <p> {{ pokemons[0].pkmName  }} </p>
+                    </div>
+                    <div class="name-class-inner">
+                        <h3>Type</h3>
+                        <div class="type-spec">
+                            <p> {{ pokemons[0].pkmType1  }} </p>
+                            <p> {{ pokemons[0].pkmType2 }} </p>
+                        </div>
+                    </div>
+                </div>
+                <h3>Stats</h3>
+                <div class="stats-container">
+                    <div class="stat-individual">
+                        <h5>Health: </h5>
+                        <div class="stat-length">
+                            <p class="stat-health" :style="{width: pokemons[0].pkmHealth + 'px' }" > {{ pokemons[0].pkmHealth  }}</p>
+                        </div>
+                    </div>
+                    <div class="stat-individual">
+                        <h5>Attack: </h5>
+                        <div class="stat-length">
+                            <p class="stat-attack" :style="{width: pokemons[0].pkmAttack + 'px' }" > {{ pokemons[0].pkmAttack  }} </p>
+                        </div>
+                    </div>
+                    <div class="stat-individual">
+                        <h5>Defense: </h5>
+                        <div class="stat-length">
+                            <p class="stat-defence" :style="{width: pokemons[0].pkmDefense + 'px' }" > {{ pokemons[0].pkmDefense  }} </p>
+                        </div>
+                    </div>
+                    <div class="stat-individual">
+                        <h5>Speed: </h5>
+                        <div class="stat-length">
+                            <p class="stat-speed" :style="{width: pokemons[0].pkmSpeed + 'px' }" > {{ pokemons[0].pkmSpeed  }} </p>
+                        </div>
+                    </div>
+                    
+                    
+                </div>
+            </div>
         </div>
         <div v-else>
             <h2>No se encontró el pokemon que buscaste</h2>
@@ -49,11 +88,155 @@
                 this.succeded.push('404');
                 console.log('error: ' + error);
             };
+        },
+        mounted() {
+            /* let pokemonEl = document.querySelectorAll('.left-sprite');
+            console.log(pokemonEl)
+            let type1val = this.pokemons
+            switch (type1val) {
+                case 'grass':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#4DAD5B"
+                    break;
+                case 'fire':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#EE8130"   
+                    break;
+                case 'water':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#1B53BA"           
+                    break;
+                case 'bug':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#A6B91A"           
+                    break;
+                case 'poison':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#A33EA1"           
+                    break;
+                case 'electric':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#F7D02C"           
+                    break;
+                case 'ground':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#E2BF65"           
+                    break;
+                case 'fairy':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#D685AD"           
+                    break;
+                case 'fighting':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#C22E28"           
+                    break;
+                case 'psychic':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#F95587"           
+                    break;
+                case 'rock':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#B6A136"           
+                    break;
+                case 'ghost':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#735797"           
+                    break;
+                case 'ice':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#96D9D6"           
+                    break;
+                case 'dragon':
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#6F35FC"           
+                    break;
+                default:
+                    pokeCardAvailable.firstChild.style.backgroundColor = "#A8A77A"   
+                    break;
+            } */
         }
     }
 </script>
 
 <style scoped>
 
+.container-pokemon-detail {
+    display: flex;
+    margin: auto;
+    justify-content: center;
+    align-items: center;
+    height: 100%;
+    width: 100%;
+    text-transform: capitalize;
+    flex-wrap: wrap;
+}
+.right-info {
+    display: flex;
+    width: 100%;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+}
+.left-sprite img {
+    width: 250px;
+    height: 250px;
+}
+.name-class {
+    display: flex;
+    flex-direction: row;
+    width: 100%;
+    justify-content: space-around;
+    height: 150px;
+}
+.name-class-inner {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 150px;
+}
+.type-spec {
+    display: flex;
+    flex-direction: row;
+}
+.stats-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    width: 100%;
+}
+.stat-individual {
+    display: flex;
+    flex-direction: row;
+    align-items: center;
+    width: 90%;
+}
+.stat-individual h5 {
+    width: 70px;
+}
+.stat-individual p {
+    width: 10px;
+    height: 20px;
+}
+.stat-length {
+    width: 255px;
+    background-color: dimgrey;
+    border-radius: 5px;
+}
+.stat-health {
+    background-color: red;
+    border-radius: 5px;
+    margin: 0;
+    padding: 0;
+    width: 0;
+}
+.stat-attack {
+    background-color: orange;
+    border-radius: 5px;
+    margin: 0;
+    padding: 0;
+    width: 0;
+}
+.stat-defence {
+    background-color: rgb(0, 174, 255);
+    border-radius: 5px;
+    margin: 0;
+    padding: 0;
+    width: 0;
+}
+.stat-speed {
+    background-color: yellow;
+    border-radius: 5px;
+    margin: 0;
+    padding: 0;
+    width: 0;
+}
 
 </style>
--- pages/detail/_name/index.vue
+++ pages/detail/_name/index.vue
@@ -14,8 +14,10 @@ export default {
 }
 </script>
 
-<style>
+<style scoped>
 .container {
     min-height: 70vh;
+    display: flex;
+    justify-content: center;
 }
 </style>
